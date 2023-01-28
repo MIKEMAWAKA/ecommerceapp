a2757f590a8926fcb630c0efd32ed256492ee2f2
mike <michaelmussa9@gmail.com>
2023-01-28 13:42:34 +0300

    home page and navbar bar page

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components"
+
+
+const Container = styled.div`
+height:30px;
+background-color: teal;
+color: white;
+
+
+`
+
+const Announcement = () => {
+  return (
+    <Container>Super Deal! Free SHipping on Orders Over 700,000Tsh</Container>
+  )
+}
+
+export default Announcement
---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Announcement from '../components/Announcement'
+
+const Home = () => {
+  return (
+    <div>
+      
+      <Announcement></Announcement>
+        <Navbar/>
+
+
+   </div>
+  )
+}
+
+
+export default Home
